1b6c2f657c8f5ddb4e582ff84f32b25884cab568
MasFlam <masflam@masflam.com>
2022-06-07 08:07:13 +0200

    Update ANTLR generation script

--- gen_antlr.sh
+++ gen_antlr.sh
@@ -2,9 +2,9 @@
 
 ANTLR_JAR=antlr/antlr-4.10.1-complete.jar
 
-rm -r src/durak/grammar
+rm -r durak/grammar
 
 cd antlr
 
-java -jar ../"$ANTLR_JAR" DurakLexer.g4 -Dlanguage=Python3 -no-listener -visitor -o ../src/durak/grammar
-java -jar ../"$ANTLR_JAR" DurakParser.g4 -Dlanguage=Python3 -no-listener -visitor -o ../src/durak/grammar
+java -jar ../"$ANTLR_JAR" DurakLexer.g4 -Dlanguage=Python3 -no-listener -visitor -o ../durak/grammar
+java -jar ../"$ANTLR_JAR" DurakParser.g4 -Dlanguage=Python3 -no-listener -visitor -o ../durak/grammar
